c662b17a6b1d7d55957fdfd443c52b241ed04778
Fayz Rahman <fayz.rahman@utexas.edu>
2012-09-21 00:03:21 -0500

    Updated tileset, along w/ accommodating load logic and Terrain

--- src/tbsgame/states/GameplayTest.java
+++ src/tbsgame/states/GameplayTest.java
@@ -24,11 +24,12 @@ public class GameplayTest extends BasicGameState {
 	public void init(GameContainer gc, StateBasedGame game)
 			throws SlickException {
 		SpriteSheet tiles = new SpriteSheet("res/tileset.png", 32, 32);
-		ResourceManager.INSTANCE.putImage(Terrain.PLAINS.getKey(), tiles.getSubImage(0, 0));
-		ResourceManager.INSTANCE.putImage(Terrain.MOUNTAINS.getKey(), tiles.getSubImage(1,0));
-		ResourceManager.INSTANCE.putImage(Terrain.FORREST.getKey(), tiles.getSubImage(2, 0));
-		ResourceManager.INSTANCE.putImage("blue_highlight", tiles.getSubImage(2, 2));
-		ResourceManager.INSTANCE.putImage("red_highlight", tiles.getSubImage(3, 2));
+		ResourceManager.INSTANCE.putImage("no_image", tiles.getSubImage(0, 0));
+		ResourceManager.INSTANCE.putImage(Terrain.PLAINS.getKey(), tiles.getSubImage(1, 0));
+		ResourceManager.INSTANCE.putImage(Terrain.MOUNTAINS.getKey(), tiles.getSubImage(2,0));
+		ResourceManager.INSTANCE.putImage(Terrain.FORREST.getKey(), tiles.getSubImage(3, 0));
+		ResourceManager.INSTANCE.putImage("blue_highlight", tiles.getSubImage(3, 2));
+		ResourceManager.INSTANCE.putImage("red_highlight", tiles.getSubImage(4, 2));
 
 		level = LevelBuilder.readLevelFile("res/testLevel.txt");
 	}
--- src/tbsgame/tiles/Terrain.java
+++ src/tbsgame/tiles/Terrain.java
@@ -3,7 +3,11 @@ package tbsgame.tiles;
 public enum Terrain {
 	PLAINS("tile_grass"),
 	FORREST("tile_forrest"),
-	MOUNTAINS("tile_mountains")
+	MOUNTAINS("tile_mountains"),
+	//TODO: Don't know quite what do do with this yet.
+	//Have them all defined here? Or just one 'path'
+	//whose image changes?
+	PATH(null)
 	;
 	
 	
